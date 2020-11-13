7c2facb713520273428f094117e22f73c2da3404
VictoriaGranja <victoriagranja@id.uff.br>
2020-11-13 17:06:41 -0300

    criando autor

---
+++ bibliotecaComunitaria/src/biblioteca/pessoas/Autor.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biblioteca.pessoas;
+
+/**
+ *
+ * @author victoria
+ */
+public class Autor extends Pessoa{
+    
+    protected String biografia;
+    protected String pais;
+    
+    protected void cadastro(String nome, String nascimento, String telefone, String biografia, String pais){
+        
+        super.cadastro(nome, nascimento, telefone);
+        this.biografia = biografia;
+        this.pais = pais;
+    
+    }
+
+    @Override
+    protected void excluir() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
