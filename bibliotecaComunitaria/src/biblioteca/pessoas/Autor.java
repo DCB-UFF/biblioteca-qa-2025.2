cce1ead7f2b615a7a4bef85dbb816241594353f6
Luam Silva <luamsilva@id.uff.br>
2020-12-05 16:43:00 -0200

    Getters and Setters Autor

--- bibliotecaComunitaria/src/biblioteca/pessoas/Autor.java
+++ bibliotecaComunitaria/src/biblioteca/pessoas/Autor.java
@@ -23,6 +23,32 @@ public class Autor extends Pessoa{
     
     }
 
+    public String getBiografia() {
+        return biografia;
+    }
+
+    public void setBiografia(String biografia) {
+        this.biografia = biografia;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+    
     @Override
     public void excluir() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
