4f6bf48e2d5a85bfde94064868afea7bc13fdfe7
VictoriaGranja <victoriagranja@id.uff.br>
2020-12-05 18:37:58 -0300

    retirando ferias

--- bibliotecaComunitaria/src/biblioteca/pessoas/Funcionario.java
+++ bibliotecaComunitaria/src/biblioteca/pessoas/Funcionario.java
@@ -13,7 +13,6 @@ public class Funcionario extends Pessoa{
     
     protected float salario;
     protected String cargo;
-    protected boolean ferias;
 
     protected void cadastro(String nome, String nascimento, String telefone, float salario, String cargo){
         
@@ -26,14 +25,6 @@ public class Funcionario extends Pessoa{
     @Override
     public void excluir() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    protected void estaFerias(){
-        if(this.ferias){
-            System.out.println("Funcionário " + this.nome + " está de férias\n");
-        }else{
-            System.out.println("Funcionário " + this.nome + " não está de férias\n");
-        }
     }
     
     protected void ajusteSalario(){}
