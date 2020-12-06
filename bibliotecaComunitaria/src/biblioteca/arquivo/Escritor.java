c43d733b67bb91de91485e3501b78a849b26a72c
Luam Silva <luamsilva@id.uff.br>
2020-12-06 16:54:52 -0200

    Leitores de arquivos prontos

--- bibliotecaComunitaria/src/biblioteca/arquivo/Escritor.java
+++ bibliotecaComunitaria/src/biblioteca/arquivo/Escritor.java
@@ -1,5 +1,7 @@
 package biblioteca.arquivo;
 
+import biblioteca.livros.Estante;
+import biblioteca.livros.Livro;
 import biblioteca.pessoas.Autor;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
@@ -11,29 +13,74 @@ import java.io.PrintWriter;
 
 public class Escritor {
     public static void escreverAutor(Autor autor) {
-           BufferedWriter bw = null;
-           String linha = autor.getNome()+","+autor.getPais();
-           try {
-               bw = new BufferedWriter(new FileWriter("autores.csv", true));
-               PrintWriter pw= new PrintWriter(bw);
-               pw.println(linha);
-               pw.flush();
-               pw.close();
-       
-
-           } catch (FileNotFoundException e) {
-               e.printStackTrace();
-           } catch (IOException e) {
-               e.printStackTrace();
-           } finally {
-               if (bw != null) {
-                   try {
-                       bw.close();
-                   } catch (IOException e) {
-                       e.printStackTrace();
-                   }
-               }   
-           }
+        BufferedWriter bw = null;
+        String linha = autor.getNome()+","+autor.getPais();
+        try {
+            bw = new BufferedWriter(new FileWriter("autores.csv", true));
+            PrintWriter pw= new PrintWriter(bw);
+            pw.println(linha);
+            pw.flush();
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bw != null) {
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }   
+        }
     }
-    
+    public static void escreveEstante(Estante estante) {
+        BufferedWriter bw = null;
+        String linha = estante.getIdEstante()+","+estante.getGenero();
+        try {
+            bw = new BufferedWriter(new FileWriter("estantes.csv", true));
+            PrintWriter pw= new PrintWriter(bw);
+            pw.println(linha);
+            pw.flush();
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bw != null) {
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }   
+        }
+    }
+    public static void escreveLivro(Livro livro) {
+        BufferedWriter bw = null;
+        String linha = livro.getAutor().getNome() + "," + livro.getTitulo() + "," + livro.getNumPaginas() + "," +
+                livro.getISBN() + "," + livro.getGenero()  + "," + livro.getEditora() + "," + 
+                livro.getEstaEmprestado() + "," + livro.getAutor().getPais();
+        try {
+            bw = new BufferedWriter(new FileWriter("livros.csv", true));
+            PrintWriter pw= new PrintWriter(bw);
+            pw.println(linha);
+            pw.flush();
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bw != null) {
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }   
+        }
+    }
 }
