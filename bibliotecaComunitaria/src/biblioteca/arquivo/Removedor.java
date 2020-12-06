9fbd56bffc9baac205c732bcf3561a4dc8606b3d
Luam Silva <luamsilva@id.uff.br>
2020-12-06 20:19:16 -0200

    Função remover livro feita

---
+++ bibliotecaComunitaria/src/biblioteca/arquivo/Removedor.java
@@ -0,0 +1,58 @@
+package biblioteca.arquivo;
+
+import biblioteca.livros.Acervo;
+import biblioteca.livros.Livro;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/* @author Luam */
+public class Removedor {
+    
+     public static void remover(Acervo acervo, Livro livroEmprestado) {
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        File antigo = new File("livros.csv");
+        File novo = new File ("temp.csv");
+        
+        try {
+            br = new BufferedReader(new FileReader(antigo));
+            bw = new BufferedWriter(new FileWriter(novo, true));
+            PrintWriter pw= new PrintWriter(bw);
+            String linha = "";
+            
+            while ((linha = br.readLine()) != null) {
+    
+                String[] livro = linha.split(",");
+                if (!livro[1].equals(livroEmprestado.getTitulo())){
+                    pw.println(linha);
+                }
+            }
+            pw.flush();  
+            pw.close();
+            br.close();
+            antigo.delete();
+            
+            File aux = new File ("livros.csv");
+            novo.renameTo(aux);
+            
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bw != null) {
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }   
+        }
+    }
+}
--- bibliotecaComunitaria/src/biblioteca/arquivo/TestesArquivo.java
+++ bibliotecaComunitaria/src/biblioteca/arquivo/TestesArquivo.java
@@ -27,20 +27,26 @@ public class TestesArquivo {
     
     System.out.println("-----------------------------------------------");
     
-    /*  Teste da Função Editar */
+    /*  Teste da Função Editar 
     Autor aluisio = new Autor("Aluísio Azevedo","Brasil");
     Livro livroEmprestado = new Livro(2, aluisio, "O cortiço","5229885", 518, "Clássico", "Cia das Letras", false);
     Editor.emprestar(acervo, livroEmprestado);
+    */
+    
+    /*  Teste da Função Editar */
+    Autor rr = new Autor("Rick Riordan","Estados Unidos");
+    Livro livroDeletado = new Livro(2, rr, "O Sangue do Olimpo","8452004", 650, "Mitologia", "Intrinseca", false);
+    Removedor.remover(acervo, livroDeletado);
     
     
-    /*  Teste da Função Escrita */
+    /*  Teste da Função Escrita 
     
-    //Escritor.escreverAutor(new Autor("Leon Toistoi", "Russia"));
-    //Escritor.escreveEstante(new Estante(55, "Literatura Russa"));
-    //Autor lucy = new Autor("Lucy Maud Montgomery","Canada");
-    //Escritor.escreveLivro(new Livro(90,lucy, "Lucy Maud Montgomery","1234",120,"Infantil","Abril",false));
+    Escritor.escreverAutor(new Autor("Leon Toistoi", "Russia"));
+    Escritor.escreveEstante(new Estante(55, "Literatura Russa"));
+    Autor lucy = new Autor("Lucy Maud Montgomery","Canada");
+    Escritor.escreveLivro(new Livro(90,lucy, "Lucy Maud Montgomery","1234",120,"Infantil","Abril",false));
     
-    
+    */
     
     
   
