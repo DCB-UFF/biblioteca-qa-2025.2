79c826c8a146268b716bde341e18514613104794
Luam <Luam@192.168.1.15>
2021-01-29 20:52:31 -0300

    Revisão final

--- bibliotecaComunitaria/src/biblioteca/pessoas/Cadastravel.java
+++ bibliotecaComunitaria/src/biblioteca/pessoas/Cadastravel.java
@@ -2,7 +2,6 @@ package biblioteca.pessoas;
 
 /* @author victoria */
 public interface Cadastravel {
-    
     public void cadastro(String nome, String nascimento, String telefone);
     public void cadastro(String nome);
     public void modificar(String nome, String nascimento, String telefone);
